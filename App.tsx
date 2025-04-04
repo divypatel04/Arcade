5f39b64a4c24a4e27bc75cd494c455a6713f276a
Divy Patel <pdivy2002@gmail.com>
2025-04-04 18:10:25 -0400

    added revenuecat SDK

--- App.tsx
+++ App.tsx
@@ -8,6 +8,10 @@ import Navigation from './src/navigation';
 import { DataProvider } from './src/context/DataContext';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { LanguageProvider } from './src/context/LanguageContext';
+import { Platform } from 'react-native';
+import Purchases, { LOG_LEVEL } from 'react-native-purchases';
+import { REVENUECAT_API_KEY } from '@env';
+
 
 
 const queryClient = new QueryClient()
@@ -17,6 +21,17 @@ function App(): React.JSX.Element {
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? '#000' : '#fff',
+  };
+
+  const configPurchases = async () => {
+    try {
+      Purchases.setLogLevel(LOG_LEVEL.VERBOSE);
+      if (Platform.OS === 'ios') {
+        Purchases.configure({apiKey: REVENUECAT_API_KEY});
+      } else if (Platform.OS === 'android') {
+        Purchases.configure({apiKey: REVENUECAT_API_KEY});
+     }
+    } catch (r) {}
   };
 
 
--- env.d.ts
+++ env.d.ts
@@ -2,4 +2,5 @@ declare module '@env' {
   export const REACT_NATIVE_PUBLIC_SUPABASE_URL: string;
   export const REACT_NATIVE_SUPABASE_ANON_KEY: string;
   export const RIOT_APIKEY: string;
+  export const REVENUECAT_API_KEY: string;
 }
